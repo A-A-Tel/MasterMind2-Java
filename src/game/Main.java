d3722b5f06c1edb42414078f8a754ef708a4fda7
zeekomkommer <anouaranthonytel@gmail.com>
2024-11-26 13:32:12 +0100

    Split functions

--- src/game/Main.java
+++ src/game/Main.java
@@ -12,69 +12,7 @@ public class Main {
     public static final char[] secretCode = new char[codeLength];
     public static final char[] codeItems = {'R', 'G', 'B', 'Y', 'P', 'O'};
 
-    // Output the current board layout
-    public static void displayBoard(char[] playerCode, char[] evaluatedCode, int guessesLeft, boolean hasWon) {
-
-        if (guessesLeft != -1) {
-            System.out.println("Guesses: " + guessesLeft + "/" + maxAttempts);
-        }
-        if (playerCode != null) {
-            System.out.println(Arrays.toString(playerCode));
-        }
-        if (evaluatedCode != null) {
-            System.out.println(Arrays.toString(evaluatedCode));
-        }
-        // Kills the process if the game has concluded
-        if (hasWon) {
-            System.out.println("Congrats, You have cracked the code!");
-            System.exit(0);
-        }
-    }
-
-    // Checks if the current code is valid
-    public static boolean isValid(char[] playerCode) {
-
-        for (char playerCodeItem : playerCode) {
-            boolean isPresent = false;
-            for (char codeItem : codeItems) {
-                if (playerCodeItem == codeItem) {
-                    isPresent = true;
-                    break;
-                }
-            }
-            if (!isPresent) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    // Evaluates the code based on MasterMind rules, also checks if the player has won
-    public static char[] evaluate(char[] playerCode) {
-
-        int correctPositions = 0;
-        char[] evaluation = new char[codeLength];
-
-        mainLoop:
-        for (int i = 0; i < codeLength; i++) {
-            if (playerCode[i] == secretCode[i]) {
-                evaluation[i] = 'B';
-                correctPositions++;
-                continue;
-            }
-            for (char codeItem : secretCode) {
-                if (codeItem == playerCode[i]) {
-                    evaluation[i] = 'W';
-                    continue mainLoop;
-                }
-            }
-            evaluation[i] = '-';
-        }
-        if (correctPositions == codeLength) {
-            displayBoard(playerCode, evaluation, -1, true);
-        }
-        return evaluation;
-    }
+    static final MasterMind code = new MasterMind();
 
     public static void main(String[] args) {
 
@@ -93,7 +31,7 @@ public class Main {
 
         // Main game loop
         for (int i = 0; i < maxAttempts; i++) {
-            displayBoard(playerCode, evaluation, i + 1, false);
+            code.displayBoard(playerCode, evaluation, i + 1, false);
             if (error) {
                 System.out.println("That was not a valid input! Please try again...");
             } else {
@@ -108,13 +46,13 @@ public class Main {
             }
 
             // Continues without costing an attempt if the input is faulty
-            error = !isValid(playerCode);
+            error = !code.isValid(playerCode);
             if (error) {
                 i--;
                 playerCode = savedPlayerCode;
                 continue;
             }
-            evaluation = evaluate(playerCode);
+            evaluation = code.evaluate(playerCode);
         }
         sc.close();
     }
